058bd46ff29c4adafa7508f02b0edd85a2e05961
Andres Borges <andresbolima@gmail.com>
2025-02-26 18:25:40 -0500

    Purchase Functionality

--- app/checkout/ShoppingCheckoutList.tsx
+++ app/checkout/ShoppingCheckoutList.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { useState } from "react";
-import { Product } from "../product-data";
+import { Product, products } from "../product-data";
+import { useRouter } from 'next/navigation';
 
-export default async function ShoppingCheckoutList({ initialCartProducts }: { initialCartProducts: Product[] }) {
+export default function ShoppingCheckoutList({ initialCartProducts }: { initialCartProducts: Product[] }) {
     const [cartProducts, setCartProducts] = useState(initialCartProducts);
     const taxPerCentage = 0.13;
+    const router = useRouter();
 
     function calculateTotalWithoutTaxes() {
         let total = 0;
@@ -21,6 +23,32 @@ export default async function ShoppingCheckoutList({ initialCartProducts }: { in
         return total * taxPerCentage;
     }
 
+    //Purchase logic
+    async function removeFromCart(productId: string) {
+        const response = await fetch(process.env.NEXT_PUBLIC_SITE_URL + '/users/1/cart', {
+            method: 'DELETE',
+            body: JSON.stringify({
+                productId
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const updatedCartProducts = await response.json();
+        setCartProducts(updatedCartProducts);
+    }
+
+    async function purchaseItems() {
+        //Removes all items from cart
+        await Promise.all(
+            cartProducts.map(product => removeFromCart(product.id))
+        );
+
+        //Redirect to thank you page
+        router.push('/');
+    }
+
     return (
         <div className="container mx-auto p-8">
             <h1 className="text-4xl font-bold mb-8">Checkout</h1>
@@ -33,20 +61,25 @@ export default async function ShoppingCheckoutList({ initialCartProducts }: { in
                 <div className="bg-white rounded-lg shadow-lg p-4 hover:shadow-xl transition duration-300 inline-block">
                     {cartProducts.map(product => (
                         <div className="mb-4">
-                            <h3 className="font-bold inline-block w-96">{product.name}</h3>
+                            <h3 className="font-bold inline-block w-96">{product.name}:</h3>
                             <span>${product.price}.00</span>
                         </div>
                     ))}
                     <div className="mb-4">
                         <h3 className="font-bold inline-block w-96">Taxes:</h3>
-                        <span>${(await calculateTaxes()).toFixed(2)}</span>
+                        <span>${(calculateTaxes()).toFixed(2)}</span>
                     </div>
                     <div className="mb-4">
                         <h3 className="font-bold inline-block w-96">Total:</h3>
                         <span>${(calculateTotalWithoutTaxes() + calculateTaxes()).toFixed(2)}</span>
                     </div>
                     <div className="flex mt-5">
-                        <a className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-5 w-40 text-center" href="/checkout">Purchase</a>
+                        <button
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-40 text-center mr-5"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                purchaseItems();
+                            }}>Purchase</button>
                         <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-40 text-center" href="/cart">Return to Cart</a>
                     </div>
                 </div>
@@ -54,4 +87,5 @@ export default async function ShoppingCheckoutList({ initialCartProducts }: { in
             }
         </div >
     )
+
 }
