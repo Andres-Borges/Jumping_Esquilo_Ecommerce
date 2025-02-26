8b999b644d175547cb50a7ee4baa906d48d8efed
Andres Borges <andresbolima@gmail.com>
2025-02-26 16:36:46 -0500

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,17 @@ export default async function Home() {
   return (
     <>
       <SetUserId />
-      <h1>This is the home page</h1>
-      <p></p>
+      <div className="flex flex-row items-center">
+        <div className="basis-1/2">
+          <img src="/EsquiloLogo.png" alt="Esquilo Logo" title="Esquilo Logo" />
+        </div>
+        <div className="basis-1/2">
+          <h1 className="text-8xl font-bold">PROGRAM</h1>
+          <h1 className="text-8xl font-bold">WITH</h1>
+          <h1 className="text-8xl font-bold text-green-500">STYLE.</h1>
+          <div className="mt-5"><a className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full mt-10" href="/products">Browse our products</a></div>
+        </div>
+      </div >
     </>
   );
 }
